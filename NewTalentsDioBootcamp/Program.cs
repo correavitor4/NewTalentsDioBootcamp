a5a1ae585478557d51d2d32e208f133d9ff00918
correa <4correavitor@gmail.com>
2024-05-05 15:08:45 -0300

    Adicionar arquivos de projeto.

---
+++ NewTalentsDioBootcamp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewTalentsDioBootcamp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
